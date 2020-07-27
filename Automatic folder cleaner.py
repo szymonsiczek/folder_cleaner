c50e552d6b3324320146d7df55eb646fca037372
Szymon Siczek <szymonsiczek@gmail.com>
2020-07-27 11:35:35 +0200

    automatic_folder_cleaner.py changed (function getSuffixWithoutDot())

--- Automatic folder cleaner.py
+++ Automatic folder cleaner.py
@@ -3,7 +3,9 @@
 # into a folders named after the type of files
 
 from pathlib import Path
-import os, shutil
+import os
+import shutil
+
 
 def findFileTypesInFolder(folder):
     finalListofFileTypes = []
@@ -13,11 +15,15 @@ def findFileTypesInFolder(folder):
             finalListofFileTypes.append(suffix)
     return finalListofFileTypes
 
-def getSuffixWithoutDot(file):
-    return Path(os.path.join(folderToClean, file)).suffix.strip('.')
-    
 
-# Ask user which folder needs to be cleaned 
+def getSuffixWithoutDot(file):
+    path_file = Path(os.path.join(folderToClean, file))
+    file_suffix = path_file.suffix
+    suffix_without_dot = file_suffix.strip('.')
+    return suffix_without_dot
+
+
+# Ask user which folder needs to be cleaned
 print('Please enter the absolute path of a folder, that needs to be cleaned:')
 folderToClean = input()
 
@@ -26,13 +32,12 @@ finalListOfFileTypes = findFileTypesInFolder(folderToClean)
 
 # Create a new folder for every type of file
 for filetype in finalListOfFileTypes:
-    newFolder =(os.path.join(folderToClean, (filetype.lower())))
+    newFolder = (os.path.join(folderToClean, (filetype.lower())))
     if not os.path.exists(newFolder):
         os.makedirs(newFolder)
 
     # Segregate files into acuurate folders
     for filename in os.listdir(folderToClean):
-        if os.path.isfile(os.path.join(folderToClean,filename)):
+        if os.path.isfile(os.path.join(folderToClean, filename)):
             if getSuffixWithoutDot(filename).lower() == filetype.lower():
-                shutil.move((os.path.join(folderToClean,filename)), newFolder)
-         
+                shutil.move((os.path.join(folderToClean, filename)), newFolder)
